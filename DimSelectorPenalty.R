15ea9da196759174623d90b2169489e22e8885af
ionelpr <123517917+ionelpr@users.noreply.github.com>
2026-03-18 17:23:28 +0100

    Refactor DimSelectorPenalty function

--- DimSelectorPenalty.R
+++ DimSelectorPenalty.R
@@ -4,19 +4,10 @@ DimSelectorPenalty <- function(out)
 {
   
   tmp1 = nrow(out)
-  fpi = min(out[, 6])		# soluzione piu' parsimoniosa (minor nfp)
+  fpi = min(out[, 6])	
   
-  # in fpi metto tutti i possibili valori distinti di nfp (dal minore al maggiore)
-  # for (i in 1:tmp1) {
-  #   if (out[i, 6] != max(fpi)) {
-  #     fpi = c(fpi, out[i, 6])
-  #   }
-  # }
   
   fpi = sort(unique(out[,6])) # 1
-  
-  # in out_best vado a mettere tutte le possibili soluzioni di interesse
-  # cioe', dato nfp, tengo soltanto la soluzione a cui corrisponde il massimo Fit
   
    tmp3 = length(fpi)
    out_best = matrix(0, tmp3, ncol(out))
@@ -43,7 +34,6 @@ DimSelectorPenalty <- function(out)
   tmp4 = nrow(out_best_best)
   
   # in out_best_best elimino eventuali soluzioni con punto sotto la linea
-  if(tmp4>2){
     i = 2
     while (i < (tmp4 - 1)) {
 	  # fit di tripla di soluzioni successive
@@ -81,43 +71,12 @@ DimSelectorPenalty <- function(out)
 	  	st[i] = Inf
       }
     }
-  }
-  if(tmp4==1){
-    st = 0
-  }
-  if(tmp4==2){
-    st = c()
-    st = c(st, out_best_best[1,4]/out_best_best[1,6])
-    st = c(st, out_best_best[2,4]/out_best_best[2,6])
-  }
+
   out_st = cbind(out_best_best, st)
   
   colnames(out_st) <- c(colnames(out),"st")
   print(out_st)
-  #print(out_st)
-  ## imax = SUM(out_st)$valueMaxc[6] # sembra non andare
   st_max = out_st[which.max(out_st[,10]), ]
-  # cat("Table: Goodness-of-fit values (%) and Scree test values st", fill = TRUE)
-  # cat("of the solutions on the higher boundary of the convex hull", fill = TRUE)
-  # tab = matrix(0, nrow = tmp4, ncol = 2)
-  # for (i in 1:tmp4) {
-	# tab[i, 1] = noquote(paste("     (", out_st[i, 1],                                 ") "))
-	# if (i == 1) {
-		# tab[i, 2] = noquote(paste("  ", round(out_st[i, 4], digits = 4), "      -"))
-	# }
-	# if (i == tmp4) {
-		# tab[i, 2] = noquote(paste("  ", round(out_st[i, 4], digits = 4), "      -"))
-	# }
-	# else {
-		# tab[i, 2] = noquote(paste("  ", round(out_st[i, 4], digits = 4), "    ", round(out_st[i, 6], digits = 2)))
-	# }
-	# if (i == imax) {
-		# suggestion = noquote(paste("The hull heuristic indicates the selection of the CP model of complexity (", out_st[i, 1], ")"))
-	# }
-  # }
-  # rownames(tab) = rep("CP", length = tmp4)
-  # colnames(tab) = c("   Complexity", "   Fit (%)      st")
-  # print(tab, quote = FALSE)
-  # cat(suggestion, fill = TRUE)
+
   return(list(out_st = out_st, st_max = st_max))
 }
